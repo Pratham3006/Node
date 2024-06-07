e650fe5963ec031f123e9b3ebd667f7e18bd49f9
Pratham3006 <2020.pratham.shetty@ves.ac.in>
2024-06-07 22:14:08 +0530

    added operation files

---
+++ Arithematic.js
@@ -0,0 +1,27 @@
+const { Console } = require("console");
+
+const a = 9;
+console.log(a);
+const b=10;
+console.log(a+b);
+function sum(a,b){
+    return a+b;
+}
+console.log(sum(90,100));
+const x=a+1;
+console.log(x);
+let y=a+1+"1";
+console.log(y);
+console.log(typeof y);
+y=y+5;
+console.log(y);
+console.log(typeof y);
+z=y*10;
+console.log(z+"10");
+m=10;
+n=m**2;
+console.log(n);
+console.log(n/10);
+console.log(n%10);
+
+
