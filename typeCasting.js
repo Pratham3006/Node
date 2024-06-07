85302ec210eea01b4f57f767d101c2aac41d1ab2
Pratham3006 <2020.pratham.shetty@ves.ac.in>
2024-06-07 21:53:30 +0530

    added typecasting

---
+++ typeCasting.js
@@ -0,0 +1,20 @@
+let num=123;
+console.log(typeof String(num));
+//as we can see that the numerc is converted to string//
+let name="Pratham";
+console.log(typeof Number(name));
+//Name to number//
+let name2="123";
+console.log(Number(name2));
+console.log(typeof Number(name2));
+console.log(parseInt(name2));
+console.log(parseFloat(name2));
+let x="a";
+console.log(typeof x);
+const item={
+    name:"Pratham",
+    marks:81,
+    rank:6
+}
+console.log(typeof item);
+console.log(typeof item[name]);
