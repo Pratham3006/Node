0a57170a418645814762f418e8d8f2d570fa7856
Pratham3006 <2020.pratham.shetty@ves.ac.in>
2024-06-08 11:01:32 +0530

    added string

---
+++ String.js
@@ -0,0 +1,16 @@
+const s="Pratham"
+const print=(w)=>{
+    console.log(w);
+}
+print(s.trim());
+print(s.slice(2))
+print(s.slice(2,5));
+print(s.toUpperCase());
+print(s.toLowerCase());
+print(s.charAt(2));
+print(s.endsWith("name"))
+print(s.includes("am"))
+print(s.length);
+print(s.replace("am","wy"));
+print(s.match("a"));
+print(s.split(" "))
